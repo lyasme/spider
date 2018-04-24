895415fc95c97aa40293a5a46055d8054165e63d
lyasme <1250823345@qq.com>
2018-04-24 14:40:49 +0800

    error

--- first/response3.py
+++ first/response3.py
@@ -1,8 +1,14 @@
-import urllib.request
+#coding:utf-8  #urllib.request == urllib2
+import urllib2  #python2
+
 
 def download(url):
-    response = urllib.request.urlopen(url,timeout=10)
+    response = urllib2.urlopen(url,timeout=5) #超时
     print(type(response))
-    print(response.info())
+    print(response.info()) #包涵网站请求的详细信息
+    print (response.read(100)) #read读取全部，read（100）读取全部
 
-print(download("http://www.baidu.com"))
+try:
+    print(download("http://www.google.com"))
+except urllib2.URLError as e:
+    print("web error",e)
