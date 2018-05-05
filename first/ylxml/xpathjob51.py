c27c96932ac1e8f99e35228da772bcca08f71295
lyasme <1250823345@qq.com>
2018-05-05 21:34:11 +0800

    xpath51job职位数

--- first/ylxml/xpathjob51.py
+++ first/ylxml/xpathjob51.py
@@ -8,11 +8,9 @@ import re
 def  download(url):
     headers={"User-Agent":"Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0);"}
     request = urllib2.Request(url,headers=headers) #请求，修改，模拟http
-
     data = urllib2.urlopen(request).read() #打开请求，抓取数据
-    # data=data.decode("gbk").encode("utf-8") #乱码
     mytree = lxml.etree.HTML(data)
-    # print (mytree.xpath("//*[@class=\"rt\"]/text()"))
+
 
     mystr = (mytree.xpath("//*[@class=\"rt\"]/text()"))[0].strip() #抓取职位个数
     print mystr
