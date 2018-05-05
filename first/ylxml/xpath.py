04041fe2ad0c276aeaf1e1d1492aba085937c335
lyasme <1250823345@qq.com>
2018-05-05 16:40:43 +0800

    属性xpath

--- first/ylxml/xpath.py
+++ first/ylxml/xpath.py
@@ -4,3 +4,25 @@ import lxml.etree
 
 html = lxml.etree.parse("index.html")
 print type(html)
+
+res = html.xpath("//li") #是一个列表，包含所有元素
+print len(res),type(res)
+print type(res[0]) #输出类型是节点
+
+print "****1*8***"
+print html.xpath("//li/@class") #输出 所有 li里面class的名称
+
+print "****2*8***"
+print html.xpath("//li/@text") #输出 所有 li里面text的名称
+
+print "****3*8***"
+print html.xpath("//li/a/@href") #输出 所有 li里面a内部href的名称
+
+print "****4*8***"
+print html.xpath("//li/a") #li下面有5个节点，每个节点对应一个元素
+
+print "****5*8***"
+print html.xpath("//li/a/@href=\"link2.html\"") #输出判断的是否有一个节点
+
+
+
